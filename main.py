3377a8c2a15e5609951ce320d7b6ecbcdf734853
Sheharyar24 <sheharyarahmedsyed@gmail.com>
2025-06-13 19:43:35 +0400

    Added ability to generate multiple passwords at the same time

--- main.py
+++ main.py
@@ -16,18 +16,23 @@ while True:
     print("1. Generate Password")
     print("2. Exit")
     print("==========================")
-    
+
     user_choice = input("\nSelect option: ")
 
     if user_choice == "1":
         password_length = int(input("\nEnter password length (min 4, max 128): "))
+
         if password_length < 4 or password_length > 128:
             print("Please enter a length between 4 and 128!\n")
-        else:
+            continue
+        
+        num_of_pass = int(input("How many passwords would you like to generate: "))
+        print("\nGenerated Password:\n")
+        for i in range(1,num_of_pass+1):
             password = Password(password_length)
             password = password.create_pass()
-            print(f"\nGenerated Password:\n{password}\n")
-
+            print(f"{i}. {password}")
+        print("")
     if user_choice == "2":
         break
-    
+
